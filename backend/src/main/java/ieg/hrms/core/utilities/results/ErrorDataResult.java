544c10e387ab0ecf089099b668ac12e03d3b157a
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-01 23:20:47 +0300

    ErrorDataResult object added

---
+++ backend/src/main/java/ieg/hrms/core/utilities/results/ErrorDataResult.java
@@ -0,0 +1,21 @@
+package ieg.hrms.core.utilities.results;
+
+public class ErrorDataResult<T> extends DataResult<T> {
+
+    public ErrorDataResult() {
+        super(null, false);
+    }
+
+    public ErrorDataResult(final String message) {
+        super(null, false, message);
+    }
+
+    public ErrorDataResult(final T data) {
+        super(data, false);
+    }
+
+    public ErrorDataResult(final T data, final String message) {
+        super(data, false, message);
+    }
+
+}
