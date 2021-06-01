82c6caa5ce07ca48594acaefe289fdfecead1e08
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-01 23:16:42 +0300

    DataResult object added

---
+++ backend/src/main/java/ieg/hrms/core/utilities/results/DataResult.java
@@ -0,0 +1,21 @@
+package ieg.hrms.core.utilities.results;
+
+public class DataResult<T> extends Result {
+
+    private final T data;
+
+    public DataResult(final T data, final boolean success) {
+        super(success);
+        this.data = data;
+    }
+
+    public DataResult(final T data, final boolean success, final String message) {
+        super(success, message);
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+}
