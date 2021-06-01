d5c0cd38a0425aa5760f036c0bc5c0d9435d9aa6
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-01 23:19:18 +0300

    SuccessResult object added

---
+++ backend/src/main/java/ieg/hrms/core/utilities/results/SuccessResult.java
@@ -0,0 +1,12 @@
+package ieg.hrms.core.utilities.results;
+
+public class SuccessResult extends Result{
+
+    public SuccessResult() {
+        super(true);
+    }
+
+    public SuccessResult(final String message) {
+        super(true, message);
+    }
+}
