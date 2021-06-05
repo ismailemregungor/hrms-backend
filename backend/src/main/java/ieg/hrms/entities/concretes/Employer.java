bb26252be8f97b3f5da8490bfaa08ce63420878e
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-05 23:40:11 +0300

    Employer object updated

--- backend/src/main/java/ieg/hrms/entities/concretes/Employer.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/Employer.java
@@ -3,7 +3,6 @@ package ieg.hrms.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,15 +14,12 @@ import lombok.NoArgsConstructor;
 @Table(name = "employers")
 public class Employer extends User {
 
-	@NotNull
 	@Column(name = "company_name")
 	private String companyName;
 
-	@NotNull
 	@Column(name = "website_address")
 	private String websiteAddress;
 
-	@NotNull
 	@Column(name = "phone_number")
 	private String phoneNumber;
 
