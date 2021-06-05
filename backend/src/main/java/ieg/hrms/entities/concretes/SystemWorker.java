3cdc68d3039fa26c21f5acaa0cc24a0d240852d6
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-05 23:38:47 +0300

    SystemWorker object updated

--- backend/src/main/java/ieg/hrms/entities/concretes/SystemWorker.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/SystemWorker.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,11 +15,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "system_worker")
 public class SystemWorker extends User {
 
-	@NotNull
 	@Column(name = "first_name")
 	private String firstName;
 
-	@NotNull
 	@Column(name = "last_name")
 	private String lastName;
 
