b2a39e673fc29ffc2d9fe7c471d86f188be8388b
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-05 23:41:52 +0300

    JobPositionManager object updated

--- backend/src/main/java/ieg/hrms/business/concretes/JobPositionManager.java
+++ backend/src/main/java/ieg/hrms/business/concretes/JobPositionManager.java
@@ -1,6 +1,8 @@
 package ieg.hrms.business.concretes;
 
 import java.util.List;
+
+import ieg.hrms.core.utilities.results.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ieg.hrms.business.abstracts.JobPositionService;
@@ -10,20 +12,30 @@ import ieg.hrms.entities.concretes.JobPosition;
 @Service
 public class JobPositionManager implements JobPositionService {
 
-	private JobPositionDao jobPositionDao;
+    private JobPositionDao jobPositionDao;
 
-	public JobPositionManager() {
+    @Autowired
+    public JobPositionManager(JobPositionDao jobPositionDao) {
+        super();
+        this.jobPositionDao = jobPositionDao;
+    }
 
-	}
+    @Override
+    public DataResult<List<JobPosition>> getAll() {
+        return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(), "Job positions listed.");
+    }
 
-	@Autowired
-	public JobPositionManager(JobPositionDao jobPositionDao) {
-		super();
-		this.jobPositionDao = jobPositionDao;
-	}
+    @Override
+    public Result add(JobPosition jobPosition) {
+        if(getByPositionName(jobPosition.getPositionName()) != null){
+            return new ErrorResult(jobPosition.getPositionName() + " already exists");
+        }
+        this.jobPositionDao.save(jobPosition);
+        return new SuccessResult("Job position has been added.");
+    }
 
-	@Override
-	public List<JobPosition> getAll() {
-		return this.jobPositionDao.findAll();
-	}
+    @Override
+    public DataResult<JobPosition> getByPositionName(String title) {
+        return new SuccessDataResult<JobPosition>(this.jobPositionDao.findByPositionName(title));
+    }
 }
