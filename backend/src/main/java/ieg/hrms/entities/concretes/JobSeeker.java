d95962691bbaa5cc6069876f01cd1fd535e06f88
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-05 23:39:20 +0300

    JobSeeker object updated

--- backend/src/main/java/ieg/hrms/entities/concretes/JobSeeker.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/JobSeeker.java
@@ -3,33 +3,30 @@ package ieg.hrms.entities.concretes;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@PrimaryKeyJoinColumn(name = "user_id")
 @Table(name = "job_seekers")
 public class JobSeeker extends User{
 	
-	@NotNull
+
 	@Column(name = "first_name")
 	private String firstName;
 	
-	@NotNull
 	@Column(name = "last_name")
 	private String lastName;
 	
-	@NotNull
 	@Column(name = "national_id_no")
 	private String nationalIdNo;
 	
-	@NotNull
 	@Column(name = "date_of_birth")
 	private Date dateOfBirth;
 
