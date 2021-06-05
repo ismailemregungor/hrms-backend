9a59ac5b301a3866f6b6d2fcd5d792b981863e36
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-05 23:39:47 +0300

    JobPosition object updated

--- backend/src/main/java/ieg/hrms/entities/concretes/JobPosition.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/JobPosition.java
@@ -6,7 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,16 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "job_position")
+@Table(name = "job_positions")
 public class JobPosition {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@NotNull
 	@Column(name = "id")
 	private int id;
 
-	@NotNull
 	@Column(name = "position_name")
 	private String positionName;
 
