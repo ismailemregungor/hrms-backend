17180f68c10535f66be3b4cf6cc45d188db3a72c
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-08 04:38:18 +0300

    JobSeekerServiceobject added

---
+++ backend/src/main/java/ieg/hrms/business/abstracts/JobSeekerService.java
@@ -0,0 +1,14 @@
+package ieg.hrms.business.abstracts;
+
+import ieg.hrms.core.utilities.results.DataResult;
+import ieg.hrms.core.utilities.results.Result;
+import ieg.hrms.entities.concretes.JobSeeker;
+import java.util.List;
+
+public interface JobSeekerService {
+
+    DataResult<List<JobSeeker>> getAll();
+
+    Result add(JobSeeker jobSeeker);
+
+}
