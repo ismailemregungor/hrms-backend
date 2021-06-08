2d96a88b28a1fbaaf8adc8fafece78cb5bdc54b4
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-08 04:37:03 +0300

    EmployerDao object added

---
+++ backend/src/main/java/ieg/hrms/dataAccess/abstracts/EmployerDao.java
@@ -0,0 +1,9 @@
+package ieg.hrms.dataAccess.abstracts;
+
+import ieg.hrms.entities.concretes.Employer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployerDao extends JpaRepository<Employer, Integer> {
+
+
+}
