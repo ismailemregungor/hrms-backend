e561fe42425dcd3d36bdce574b8c6b1a665aff98
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-08 04:36:17 +0300

    JobPositionDao object updated

--- backend/src/main/java/ieg/hrms/dataAccess/abstracts/JobPositionDao.java
+++ backend/src/main/java/ieg/hrms/dataAccess/abstracts/JobPositionDao.java
@@ -2,9 +2,8 @@ package ieg.hrms.dataAccess.abstracts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ieg.hrms.entities.concretes.JobPosition;
-import java.util.List;
 
 public interface JobPositionDao extends JpaRepository<JobPosition, Integer> {
 
-    JobPosition findByPositionName(String title);
+    // JobPosition findByPositionName(String positionName);
 }
