f2dcee459d74f4fffcb44ab088f207532b1af88d
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-08 04:36:40 +0300

    JobSeekerDao object added

---
+++ backend/src/main/java/ieg/hrms/dataAccess/abstracts/JobSeekerDao.java
@@ -0,0 +1,9 @@
+package ieg.hrms.dataAccess.abstracts;
+
+import ieg.hrms.entities.concretes.JobSeeker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerDao extends JpaRepository<JobSeeker, Integer> {
+
+
+}
