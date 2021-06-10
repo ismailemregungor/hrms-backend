d8c0443c2d87271c94fb288b1c508bc72d3c2ab0
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-10 23:52:27 +0300

    JobAdvert object added

---
+++ backend/src/main/java/ieg/hrms/entities/concretes/JobAdvert.java
@@ -0,0 +1,53 @@
+package ieg.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "job_adverts")
+@AllArgsConstructor
+public class JobAdvert {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "job_description")
+    private String jobDescription;
+
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    @Column(name = "max_salary")
+    private int maxSalary;
+
+    @Column(name = "active_job_position_count")
+    private int activeJobPositionCount;
+
+    @Column(name = "is_job_position_active")
+    private boolean isJobPositionActive;
+
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Column(name = "expiration_date")
+    private Date expirationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "job_position_id")
+    private JobPosition jobPosition;
+
+    @ManyToOne
+    @JoinColumn(name = "employer_id")
+    private Employer employer;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+}
