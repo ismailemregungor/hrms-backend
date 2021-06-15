3e118f71cf720db86c5fd4574b7e66842dff8c02
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-15 23:41:05 +0300

    ImageCV object added

---
+++ backend/src/main/java/ieg/hrms/entities/concretes/ImageCV.java
@@ -0,0 +1,29 @@
+package ieg.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "job_seekers_image_cv")
+public class ImageCV {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @ManyToOne()
+    @JoinColumn(name = "job_seeker_id")
+    private JobSeeker jobSeeker;
+
+}
