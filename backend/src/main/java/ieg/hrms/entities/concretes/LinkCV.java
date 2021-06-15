1fb8f5690c3792adbe0a2c189c09b00efa0572ec
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-15 23:37:55 +0300

    LinkCV object added

---
+++ backend/src/main/java/ieg/hrms/entities/concretes/LinkCV.java
@@ -0,0 +1,33 @@
+package ieg.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "job_seekers_link_cv")
+public class LinkCV {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "link_name")
+    private String linkName;
+
+    @Column(name = "link_url")
+    private String linkUrl;
+
+    @ManyToOne()
+    @JoinColumn(name = "job_seeker_id")
+    private JobSeeker jobSeeker;
+
+
+}
