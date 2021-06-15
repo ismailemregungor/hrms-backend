7701783612ec96dfe154317fd68ea92b6dff9beb
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-15 23:39:56 +0300

    SkillCV object added

---
+++ backend/src/main/java/ieg/hrms/entities/concretes/SkillCV.java
@@ -0,0 +1,29 @@
+package ieg.hrms.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "job_seekers_skill_cv")
+public class SkillCV {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "programming_language")
+    private String programmingLanguage;
+
+    @ManyToOne()
+    @JoinColumn(name = "job_seeker_id")
+    private JobSeeker jobSeeker;
+
+}
