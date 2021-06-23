55ef7e07a05bfb0cb14ed898c74eb3e61ca72519
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-23 23:28:51 +0300

    Update User object

--- backend/src/main/java/ieg/hrms/entities/concretes/User.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/User.java
@@ -13,15 +13,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
-	@Column(name = "email")
-	private String email;
+    @Column(name = "email")
+    private String email;
 
-	@Column(name = "password")
-	private String password;
+    @Column(name = "password")
+    private String password;
 
 }
