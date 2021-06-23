49cb41b083c4e0f563f507eb097aa6b58a2d67d9
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-23 23:46:26 +0300

    Update JobSeeker object

--- backend/src/main/java/ieg/hrms/entities/concretes/JobSeeker.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/JobSeeker.java
@@ -1,30 +1,59 @@
 package ieg.hrms.entities.concretes;
 
 import java.util.Date;
+import java.util.List;
 import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode(callSuper = false)
+@PrimaryKeyJoinColumn(name = "user_id")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "job_seekers")
-public class JobSeeker extends User{
+public class JobSeeker extends User {
 
-	@Column(name = "first_name")
-	private String firstName;
-	
-	@Column(name = "last_name")
-	private String lastName;
-	
-	@Column(name = "national_id_no")
-	private String nationalIdNo;
-	
-	@Column(name = "date_of_birth")
-	private Date dateOfBirth;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "national_id_no")
+    private String nationalIdNo;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobSeeker")
+    private List<EducationCV> educationCV;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobSeeker")
+    private List<ExperienceCV> experienceCV;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobSeeker")
+    private List<SkillCV> skillCV;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobSeeker")
+    private List<LanguageCV> languageCV;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobSeeker")
+    private List<LinkCV> linkCV;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobSeeker", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CoverLetterCV> coverLetterCV;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "jobSeeker", optional = false, fetch = FetchType.LAZY)
+    private ImageCV imageCV;
 
 }
