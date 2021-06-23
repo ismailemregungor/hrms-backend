fc1040682041a867ac25b73e18e33feee10b1c5c
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-23 23:43:30 +0300

    Update SystemWorker object

--- backend/src/main/java/ieg/hrms/entities/concretes/SystemWorker.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/SystemWorker.java
@@ -2,24 +2,26 @@ package ieg.hrms.entities.concretes;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@PrimaryKeyJoinColumn(name = "user_id")
 @EqualsAndHashCode(callSuper = false)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "system_worker")
+@Table(name = "system_workers")
 public class SystemWorker extends User {
 
-	@Column(name = "first_name")
-	private String firstName;
+    @Column(name = "first_name")
+    private String firstName;
 
-	@Column(name = "last_name")
-	private String lastName;
+    @Column(name = "last_name")
+    private String lastName;
 
 }
