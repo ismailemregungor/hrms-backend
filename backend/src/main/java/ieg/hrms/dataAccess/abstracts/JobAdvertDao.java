a9ddafbe1dce1f0967734f871179a3163c343b7d
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-25 23:38:10 +0300

    Update JobAdvertDao object

--- backend/src/main/java/ieg/hrms/dataAccess/abstracts/JobAdvertDao.java
+++ backend/src/main/java/ieg/hrms/dataAccess/abstracts/JobAdvertDao.java
@@ -3,6 +3,7 @@ package ieg.hrms.dataAccess.abstracts;
 import ieg.hrms.entities.concretes.JobAdvert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface JobAdvertDao extends JpaRepository<JobAdvert, Integer> {
