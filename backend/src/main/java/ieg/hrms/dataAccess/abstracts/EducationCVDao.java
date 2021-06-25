1a753fdf761ab804bae0cdb77af542517b179de7
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-25 23:39:50 +0300

    Update EducationCVDao object

--- backend/src/main/java/ieg/hrms/dataAccess/abstracts/EducationCVDao.java
+++ backend/src/main/java/ieg/hrms/dataAccess/abstracts/EducationCVDao.java
@@ -2,7 +2,14 @@ package ieg.hrms.dataAccess.abstracts;
 
 import ieg.hrms.entities.concretes.EducationCV;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EducationCVDao extends JpaRepository<EducationCV, Integer> {
+
+    EducationCV getById(int id);
+
+    List<EducationCV> getByJobSeekerId(int id);
+
+    List<EducationCV> getByJobSeekerIdOrderByEndDateDesc(int id);
 
 }
