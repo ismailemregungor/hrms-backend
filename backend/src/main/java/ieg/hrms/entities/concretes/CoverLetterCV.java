a900c1fe8d82b8fb05ed1d337b2defbba352cdd2
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-06-27 23:53:29 +0300

    Update CoverLetterCV object

--- backend/src/main/java/ieg/hrms/entities/concretes/CoverLetterCV.java
+++ backend/src/main/java/ieg/hrms/entities/concretes/CoverLetterCV.java
@@ -1,14 +1,12 @@
 package ieg.hrms.entities.concretes;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "job_seekers_cover_letter_cv")
 public class CoverLetterCV {
@@ -21,15 +19,15 @@ public class CoverLetterCV {
     @Column(name = "content")
     private String content;
 
+    @ManyToOne()
+    @JsonIgnore
+    @JoinColumn(name = "job_seeker_id")
+    private JobSeeker jobSeeker;
+
     public CoverLetterCV(String content, int jobSeekerId) {
         super();
         this.content = content;
         this.jobSeeker.setId(jobSeekerId);
     }
-
-    @ManyToOne()
-    @JsonIgnore
-    @JoinColumn(name = "job_seeker_id")
-    private JobSeeker jobSeeker;
 
 }
