6bb6a8ac2a20ce40e228ee4e0ff39a7e33cd6030
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-04 15:52:59 +0300

    Update SkillCVDao object

--- backend/src/main/java/ieg/hrms/dataAccess/abstracts/SkillCVDao.java
+++ backend/src/main/java/ieg/hrms/dataAccess/abstracts/SkillCVDao.java
@@ -2,7 +2,6 @@ package ieg.hrms.dataAccess.abstracts;
 
 import ieg.hrms.entities.concretes.SkillCV;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface SkillCVDao extends JpaRepository<SkillCV, Integer> {
