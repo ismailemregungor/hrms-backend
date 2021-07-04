04eac454804c67390648f0ec8b5d7361ac71a436
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-04 16:07:51 +0300

    Add JobSeekerCVDto object

---
+++ backend/src/main/java/ieg/hrms/entities/dtos/JobSeekerCVDto.java
@@ -0,0 +1,17 @@
+package ieg.hrms.entities.dtos;
+
+import ieg.hrms.entities.concretes.*;
+import java.util.List;
+
+public class JobSeekerCVDto {
+
+    public JobSeeker jobSeeker;
+    public List<EducationCV> educationCV;
+    public List<ExperienceCV> experienceCV;
+    public List<SkillCV> skillCV;
+    public List<LanguageCV> languageCV;
+    public List<LinkCV> linkCV;
+    public List<CoverLetterCV> coverLetterCV;
+    public ImageCV imageCV;
+
+}
