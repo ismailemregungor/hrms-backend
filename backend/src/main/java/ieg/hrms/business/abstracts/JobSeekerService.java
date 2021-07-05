294a475c859f81c720d6276f9a4a6df4c2eb26a2
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-05 18:45:16 +0300

    Update JobSeekerService object

--- backend/src/main/java/ieg/hrms/business/abstracts/JobSeekerService.java
+++ backend/src/main/java/ieg/hrms/business/abstracts/JobSeekerService.java
@@ -3,16 +3,23 @@ package ieg.hrms.business.abstracts;
 import ieg.hrms.core.utilities.results.DataResult;
 import ieg.hrms.core.utilities.results.Result;
 import ieg.hrms.entities.concretes.JobSeeker;
+import ieg.hrms.entities.dtos.JobSeekerCVDto;
 import java.util.List;
 
 public interface JobSeekerService {
 
-    DataResult<List<JobSeeker>> getAll();
-
     Result add(JobSeeker jobSeeker);
 
-    //Result update(JobSeeker jobSeeker);
+    Result update(JobSeeker jobSeeker);
 
-    //Result delete(JobSeeker jobSeeker);
+    Result delete(int id);
+
+    DataResult<List<JobSeeker>> getAll();
+
+    DataResult<JobSeeker> getById(int id);
+
+    DataResult<JobSeekerCVDto> getJobSeekerCVById(int id);
+
+    DataResult<JobSeeker> getJobSeekerByNationalIdNo(String nationalIdNo);
 
 }
