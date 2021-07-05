33875bfc9f33a31063e97e84e0a3b134a7625c08
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-05 18:42:58 +0300

    Update EducationCVService object

--- backend/src/main/java/ieg/hrms/business/abstracts/EducationCVService.java
+++ backend/src/main/java/ieg/hrms/business/abstracts/EducationCVService.java
@@ -1,5 +1,24 @@
 package ieg.hrms.business.abstracts;
 
+import ieg.hrms.core.utilities.results.DataResult;
+import ieg.hrms.core.utilities.results.Result;
+import ieg.hrms.entities.concretes.EducationCV;
+import java.util.List;
+
 public interface EducationCVService {
+
+    Result add(EducationCV educationCV);
+
+    Result update(EducationCV educationCV);
+
+    Result delete(int id);
+
+    DataResult<List<EducationCV>> getAll();
+
+    DataResult<EducationCV> getById(int id);
+
+    DataResult<List<EducationCV>> getByJobSeekerId(int id);
+
+    DataResult<List<EducationCV>> getByJobSeekerIdOrderByEndDateDesc(int id);
 
 }
