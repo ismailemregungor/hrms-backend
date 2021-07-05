cc38a6a412620d793b3649630a18c63e7b720a10
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-05 18:26:20 +0300

    Update JobSeekerCVDto object

--- backend/src/main/java/ieg/hrms/entities/dtos/JobSeekerCVDto.java
+++ backend/src/main/java/ieg/hrms/entities/dtos/JobSeekerCVDto.java
@@ -1,17 +1,23 @@
 package ieg.hrms.entities.dtos;
 
 import ieg.hrms.entities.concretes.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobSeekerCVDto {
 
-    public JobSeeker jobSeeker;
-    public List<EducationCV> educationCV;
-    public List<ExperienceCV> experienceCV;
-    public List<SkillCV> skillCV;
-    public List<LanguageCV> languageCV;
-    public List<LinkCV> linkCV;
-    public List<CoverLetterCV> coverLetterCV;
-    public ImageCV imageCV;
+    private JobSeeker jobSeeker;
+    private List<EducationCV> educationCV;
+    private List<ExperienceCV> experienceCV;
+    private List<SkillCV> skillCV;
+    private List<LanguageCV> languageCV;
+    private List<LinkCV> linkCV;
+    private List<CoverLetterCV> coverLetterCV;
+    private List<ImageCV> imageCV;
 
 }
