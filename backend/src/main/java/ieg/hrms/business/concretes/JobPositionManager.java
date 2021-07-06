9376999b8aee3bde2383cea1a328f7e1f8b5997c
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-06 19:09:09 +0300

    Update JobPositionManager object

--- backend/src/main/java/ieg/hrms/business/concretes/JobPositionManager.java
+++ backend/src/main/java/ieg/hrms/business/concretes/JobPositionManager.java
@@ -1,13 +1,12 @@
 package ieg.hrms.business.concretes;
 
-import java.util.List;
 import ieg.hrms.core.utilities.results.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ieg.hrms.business.abstracts.JobPositionService;
 import ieg.hrms.dataAccess.abstracts.JobPositionDao;
 import ieg.hrms.entities.concretes.JobPosition;
-
+import java.util.List;
 
 @Service
 public class JobPositionManager implements JobPositionService {
@@ -21,19 +20,41 @@ public class JobPositionManager implements JobPositionService {
     }
 
     @Override
+    public Result add(JobPosition jobPosition) {
+        for (JobPosition jobTitle : this.jobPositionDao.findAll()) {
+            if (jobTitle.getPositionName().equals(jobPosition.getPositionName())) {
+                return new ErrorResult(jobPosition.getPositionName() + " Already exist! Please add a different position.");
+            }
+        }
+        this.jobPositionDao.save(jobPosition);
+        return new SuccessResult(jobPosition.getPositionName() + " Job Position has added.");
+    }
+
+    @Override
+    public Result update(JobPosition jobPosition) {
+        this.jobPositionDao.save(jobPosition);
+        return new SuccessResult("Job Position has updated.");
+    }
+
+    @Override
+    public Result delete(int id) {
+        this.jobPositionDao.deleteById(id);
+        return new SuccessResult("Job Position has deleted.");
+    }
+
+    @Override
     public DataResult<List<JobPosition>> getAll() {
         return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll());
     }
 
     @Override
-    public Result add(JobPosition jobPosition) {
-        for (JobPosition jobTitle : this.jobPositionDao.findAll()){
-            if (jobTitle.getPositionName().equals(jobPosition.getPositionName())){
-                return new ErrorResult(jobPosition.getPositionName() + " Already exist! Please add a different position.");
-            }
-        }
-        this.jobPositionDao.save(jobPosition);
-        return new SuccessResult(jobPosition.getPositionName() + " The position has been successfully added: " );
+    public DataResult<JobPosition> getById(int id) {
+        return new SuccessDataResult<JobPosition>(this.jobPositionDao.getById(id));
+    }
+
+    @Override
+    public DataResult<JobPosition> getByPositionName(String positionName) {
+        return new SuccessDataResult<JobPosition>(this.jobPositionDao.getByPositionName(positionName));
     }
 
 }
