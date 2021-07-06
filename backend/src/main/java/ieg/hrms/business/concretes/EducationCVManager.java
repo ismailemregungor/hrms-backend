b189c6e5f1ab4d6994f4bd0e3d3ec9c934ea5751
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-06 19:13:37 +0300

    Update EducationCVManager object

--- backend/src/main/java/ieg/hrms/business/concretes/EducationCVManager.java
+++ backend/src/main/java/ieg/hrms/business/concretes/EducationCVManager.java
@@ -1,5 +1,62 @@
 package ieg.hrms.business.concretes;
 
-public class EducationCVManager {
+import ieg.hrms.business.abstracts.EducationCVService;
+import ieg.hrms.core.utilities.results.DataResult;
+import ieg.hrms.core.utilities.results.Result;
+import ieg.hrms.core.utilities.results.SuccessDataResult;
+import ieg.hrms.core.utilities.results.SuccessResult;
+import ieg.hrms.dataAccess.abstracts.EducationCVDao;
+import ieg.hrms.entities.concretes.EducationCV;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
+public class EducationCVManager implements EducationCVService {
+
+    private EducationCVDao educationCVDao;
+
+    @Autowired
+    public EducationCVManager(EducationCVDao educationCVDao) {
+        super();
+        this.educationCVDao = educationCVDao;
+    }
+
+    @Override
+    public Result add(EducationCV educationCV) {
+        this.educationCVDao.save(educationCV);
+        return new SuccessResult("Job Seeker CV Education has added.");
+    }
+
+    @Override
+    public Result update(EducationCV educationCV) {
+        this.educationCVDao.save(educationCV);
+        return new SuccessResult("Job Seeker CV Education has updated.");
+    }
+
+    @Override
+    public Result delete(int id) {
+        this.educationCVDao.deleteById(id);
+        return new SuccessResult("Job Seeker CV Education has deleted.");
+    }
+
+    @Override
+    public DataResult<List<EducationCV>> getAll() {
+        return new SuccessDataResult<List<EducationCV>>(this.educationCVDao.findAll());
+    }
+
+    @Override
+    public DataResult<EducationCV> getById(int id) {
+        return new SuccessDataResult<EducationCV>(this.educationCVDao.getById(id));
+    }
+
+    @Override
+    public DataResult<List<EducationCV>> getByJobSeekerId(int id) {
+        return new SuccessDataResult<List<EducationCV>>(this.educationCVDao.getByJobSeekerId(id));
+    }
+
+    @Override
+    public DataResult<List<EducationCV>> getByJobSeekerIdOrderByEndDateDesc(int id) {
+        return new SuccessDataResult<List<EducationCV>>(this.educationCVDao.getByJobSeekerIdOrderByEndDateDesc(id));
+    }
 }
