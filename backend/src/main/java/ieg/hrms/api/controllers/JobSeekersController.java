0a8e504ecc4a1cc5a404333340adb831f7a82b96
ismailemregungor <54360620+ismailemregungor@users.noreply.github.com>
2021-07-07 18:45:43 +0300

    Update JobSeekersController object

--- backend/src/main/java/ieg/hrms/api/controllers/JobSeekersController.java
+++ backend/src/main/java/ieg/hrms/api/controllers/JobSeekersController.java
@@ -4,28 +4,56 @@ import ieg.hrms.business.abstracts.JobSeekerService;
 import ieg.hrms.core.utilities.results.DataResult;
 import ieg.hrms.core.utilities.results.Result;
 import ieg.hrms.entities.concretes.JobSeeker;
+import ieg.hrms.entities.dtos.JobSeekerCVDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/jobseekers")
+@RequestMapping("/api/jobSeekers")
 public class JobSeekersController {
 
     private JobSeekerService jobSeekerService;
 
     @Autowired
     public JobSeekersController(JobSeekerService jobSeekerService) {
+        super();
         this.jobSeekerService = jobSeekerService;
-    }
-
-    @GetMapping("/getall")
-    public DataResult<List<JobSeeker>> getAll() {
-        return this.jobSeekerService.getAll();
     }
 
     @PostMapping("/add")
     public Result add(@RequestBody JobSeeker jobSeeker) {
         return this.jobSeekerService.add(jobSeeker);
     }
+
+    @PostMapping("/update")
+    public Result update(@RequestBody JobSeeker jobseeker) {
+        return this.jobSeekerService.update(jobseeker);
+    }
+
+    @PostMapping("/delete")
+    public Result delete(@RequestParam("id") int id) {
+        return this.jobSeekerService.delete(id);
+    }
+
+    @GetMapping("/getAll")
+    public DataResult<List<JobSeeker>> getAll() {
+        return this.jobSeekerService.getAll();
+    }
+
+    @GetMapping("/getById")
+    public DataResult<JobSeeker> getById(@RequestParam("id") int id) {
+        return this.jobSeekerService.getById(id);
+    }
+
+    @GetMapping("/getJobSeekerCVById")
+    public DataResult<JobSeekerCVDto> getJobSeekerCVById(@RequestParam("id") int id) {
+        return this.jobSeekerService.getJobSeekerCVById(id);
+    }
+
+    @GetMapping("/getJobSeekerByNationalIdNo")
+    public DataResult<JobSeeker> getJobSeekerByNationalIdNo(@RequestParam("nationalIdNo") String nationalIdNo) {
+        return this.jobSeekerService.getJobSeekerByNationalIdNo(nationalIdNo);
+    }
+
 }
